ed88ac76e1338b7b2979995b73734316574afd0a
Subham <subham.ghosh93@gmail.com>
2015-04-03 02:31:07 +0530

    read virtual and swap memory in admin

--- DS/server/views.py
+++ DS/server/views.py
@@ -7,20 +7,35 @@ import requests
 import time
 import os
 
+from server_utils import *
+
 import psutil
 
 @csrf_exempt
 def index(request):
+
+	dirWhereItWillSave = '/home/subham/DS/'
+
 	if request.method == 'POST':
-#		print request.META['HTTP_SAM']
-#		print request.body
+		ipAddrOfPOST = str(request.META['REMOTE_ADDR'])
+		
+		# Save the file sent
 		for filename, file in request.FILES.iteritems():
 			name = request.FILES[filename].name
-			print name
+			fileToSave = request.FILES[filename]
+			with open(dirWhereItWillSave + name, 'wb+') as destination:
+				for chunk in fileToSave.chunks():
+					destination.write(chunk)
+
+		#read virtual and swap memories
+		fileReceived = open(dirWhereItWillSave + name, "r+")
+		virtualMemory, swapMemory = fileReceived.read().split('\n')[0:2]
+		fileReceived.close()
+		
 		return HttpResponse("Got POST")
 	elif request.method == 'GET':
 		print request
-		return HttpResponse("Dipu is great")
+		# return HttpResponse("Dipu is great")
 	else:
 		raise Http404
 		return HttpResponse("failed")
