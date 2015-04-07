64c59f00a1249c41abf926208757f482587533c6
dipayan92 <dipayan1992@gmail.com>
2015-04-07 15:47:04 +0530

    Changes to job handler

--- job_handler.py
+++ job_handler.py
@@ -4,19 +4,6 @@ import os
 import threading
 from data import *
 from server_utils import *
-# this function should automatically create a thread but then how to handle the response
-# def submit_jobs(job,client_id):
-# 	# We should also send the jobId to the user so we can know which job was sent
-# 	urlc = "http://"+str(ListofIP[client_id])
-# 	command  = job[3]
-# 	filename = job[2]
-# 	payload = {'Command': command, 'jobid' = job[0]}
-# 	files = {'file':open(filename,'rb')}
-# 	r = requests.post(url,files = files,data = payload, proxies = proxyDict)
-	
-# 	#read job json file
-# 	# write r back to job files
-# 	#write to json file
 
 class Submit_Jobs(threading.Thread):
 	    
@@ -36,13 +23,6 @@ class Submit_Jobs(threading.Thread):
 	    	#read jobs from file
 	    	#write job back to job dict
 	    	# write back to the json file
-
-
-
-    
-	
-
-
 
 
 LastClientUsed = 0
@@ -65,37 +45,17 @@ while True:
 		t = Submit_Jobs(pending_jobs[pendingJobList[0]], LastClientUsed)
 		t. start()
 		#This modifies the json file of job
+		# lock the file
+		# download the file 
 		del pending_jobs[pendingJobList[0]]
 		jobs[pendingJobList[0]][4] = 'started'
 		jobs[pendingJobList[0]][1] = clientid
 		del pendingJobList[0]
 		saveAsJson(jobs,"jobs")
+		# unlock the file
 	time.sleep(3)
 
 
-
-
-
-
-
-
-
-# pending_jobs = {}
-# ListIP = []
-# Client = {} # has the mem space for each client
-# pendingJobList = []
-# for job in jobs:
-# 	if  jobs[job][4] == 'pending' or jobs[job][4] == 'failed':
-# 		pending_jobs[job] = jobs[job]
-# 		pendingJobList.append(job)
-# #check round robin
-# for clientid in Client:
-# 	if Client[clientid][0] > 15MB:
-# 		if submit_jobs(pending_jobs[pendingJobList[0]],clientid) == True:
-			# del pending_jobs[pendingJobList[0]]
-			# jobs[pendingJobList[0]][4] = 'started'
-			# jobs[pendingJobList[0]][1] = clientid
-			# del pendingJobList[0]
 
 
 
