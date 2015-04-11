a8a17e731559a74f0cb08ae214782e61d6288a6e
dipayan92 <dipayan1992@gmail.com>
2015-04-12 01:31:09 +0530

    changes

--- data.py
+++ data.py
@@ -9,7 +9,7 @@ users = {'admin':'admin123'}
 PrimIP = "localhost:8000"
 
 MainServerIP = "http://" + PrimIP
-SecondaryServerIP = "localhost:7001"
+SecondaryServerIP = "10.140.150.171:8004"
 # SecondaryServerIP = "http://localhost:7000"
 
 # psutilFile = "/home/dipayan/Desktop/Distributed-Systems-Project/psutilFile.json"
